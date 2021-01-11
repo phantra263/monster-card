4f8237d1274e394b9689c2e7489cf9c1638abf3d
tra.phan <phantra2212@gmail.com>
2021-01-11 17:15:10 +0700

    delete console

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
   onSearchChange = (event) => {};
   handleChange = (e) =>{
     this.setState({searchField : e.target.value});
-    console.log("ccccc");
    }
   render() {
     const {monsters, searchField}=this.state;
